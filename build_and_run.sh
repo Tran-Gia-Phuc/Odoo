3cf8bbcb696dc8d64fa62e278b270a67fc380c97
Tran-Gia-Phuc <giaphuc031204@gmail.com>
2026-05-19 14:44:23 +0700

    fix: update deploy script

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,11 +5,10 @@ echo "================================================"
 echo "=== BẮT ĐẦU DEPLOY ODOO 17 ==="
 echo "================================================"
 
-echo "=== [1/3] Pulling latest code ==="
-cd /zoo17
-git pull origin main
+echo "=== [1/3] Sync code từ Jenkins workspace vào /zoo17 ==="
+cp -r $WORKSPACE/addons/. /zoo17/addons/
 
-echo "=== [2/3] Restart Odoo only ==="
+echo "=== [2/3] Restart Odoo ==="
 docker restart zoo17-odoo17-1
 
 echo "=== [3/3] Check containers ==="
